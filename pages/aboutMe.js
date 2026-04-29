7d0f48e4ace41514a414ffc2d568546a81e9f104
lwalterson <laurenbwalterson@gmail.com>
2026-04-29 00:31:20 -0400

    redirect error fix attempt 2

--- pages/aboutMe.js
+++ pages/aboutMe.js
@@ -12,6 +12,9 @@ export function AboutMe({navigation}) {
              />
              <View>
      <Text style={styles.text}>Hello I am Lauren Walterson a senior Digital Media (Web and Interactive Media) student at UCF. My degree has given my skills in both coding and design, though I am more comfortable on the design side of things. I am familiar with HTML, CSS, Javascript, and React Native, and have small amounts of experience in PHP and Dart. My greatest interest is in film, writing and video editing and I am currently pursuing those goals with an internship at AdventHealth. </Text>
+    <TouchableOpacity  onPress={() => navigation.navigate("home")}>
+        <Text >Contact Me</Text>
+      </TouchableOpacity>
     </View>
     </View>
   );
