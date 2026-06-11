a13cfe2971c7898753f02193dbb6610219b42220
lwalterson <laurenbwalterson@gmail.com>
2026-06-11 09:00:11 -0400

    more pages

---
+++ pages/projects/bookTalk.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:79a6b253525f7fb81cd5dfd2cbf86399d0049c681b02455e76c815051054d095
+size 2336
--- pages/projects/resumeChecker.js
+++ pages/projects/resumeChecker.js
@@ -1,33 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
- import { Video, ResizeMode } from 'expo-av';
-
-export function ResumeChecker({navigation}) {
-  return (
-    <View style={styles.container}>
-     <Video  
-    source={require('../../assets/GWCGroup52026Demo.mp4')}
-    style={{ width: '100%', height: '10%' }}
-    useNativeControls
-    resizeMode={ResizeMode.CONTAIN}
-    isLooping
-        />
-     <Text>Double Tap for Video Demo</Text>
-     <Text style={styles.text}>This was a group project I participated in as a part of Girls Who Code.  It is an AI tool that analyses a resume. I was in charge of the visual design and branding.</Text>
-    </View>
-  );
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    backgroundColor: '#3e78a4'
-  },
- text: {
-    fontSize: '150%',
-     textAlign: 'center',
-  },
-});
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3cc9fbaee3af1498134ef1812a7380fd120c5706841cdce3695f65346931c6a
+size 975
